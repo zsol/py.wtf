7153de6be2a9fcdf6a7f53e68adbbd6be7226fc0
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-08-09 09:02:57 +0100

    pre-commit

--- www/lib/docs.ts
+++ www/lib/docs.ts
@@ -1,64 +1,70 @@
 import path from "path";
-import fs from 'fs';
-const indexDirectory = path.join(process.cwd(), '..', 'index');
+import fs from "fs";
+const indexDirectory = path.join(process.cwd(), "..", "index");
 
 export function getPackageIndex() {
-    const fileNames = fs.readdirSync(indexDirectory).filter((fname) => fname.endsWith('.json'));
-    return fileNames.map((name) => ({ name: name.replace(/\.json$/, ''), version: "unknown", modules: JSON.parse(fs.readFileSync(`${indexDirectory}/${name}`).toString()).modules }));
+  const fileNames = fs
+    .readdirSync(indexDirectory)
+    .filter((fname) => fname.endsWith(".json"));
+  return fileNames.map((name) => ({
+    name: name.replace(/\.json$/, ""),
+    version: "unknown",
+    modules: JSON.parse(fs.readFileSync(`${indexDirectory}/${name}`).toString())
+      .modules,
+  }));
 }
 
 export type Pkg = {
-    name: string,
-    version: string,
-    documentation: Array<Documentation>,
-    modules: Array<Module>
+  name: string;
+  version: string;
+  documentation: Array<Documentation>;
+  modules: Array<Module>;
 };
 
 export type Documentation = string;
 export type Module = {
-    name: string,
-    documentation: Array<Documentation>,
-    functions: Array<Func>,
-    variables: Array<Variable>,
-    classes: Array<Class>,
-}
+  name: string;
+  documentation: Array<Documentation>;
+  functions: Array<Func>;
+  variables: Array<Variable>;
+  classes: Array<Class>;
+};
 
 export type Func = {
-    name: string,
-    asynchronous: boolean,
-    params: Array<Param>
-    returns: Typ | null,
-    documentation: Array<Documentation>,
-}
+  name: string;
+  asynchronous: boolean;
+  params: Array<Param>;
+  returns: Typ | null;
+  documentation: Array<Documentation>;
+};
 
 export type Param = {
-    name: string,
-    type: Typ | null,
-    default: string | null,
-}
+  name: string;
+  type: Typ | null;
+  default: string | null;
+};
 
 export type Typ = string;
 
 export type Variable = {
-    name: string,
-    type: Typ | null,
-    documentation: Array<Documentation>,
-}
+  name: string;
+  type: Typ | null;
+  documentation: Array<Documentation>;
+};
 
 export type Class = {
-    name: string,
-    bases: Array<string>,
-    methods: Array<Func>,
-    class_variables: Array<Variable>,
-    instance_variables: Array<Variable>,
-    inner_classes: Array<Class>,
-    documentation: Array<Documentation>,
-}
-
+  name: string;
+  bases: Array<string>;
+  methods: Array<Func>;
+  class_variables: Array<Variable>;
+  instance_variables: Array<Variable>;
+  inner_classes: Array<Class>;
+  documentation: Array<Documentation>;
+};
 
 export function getPackage(name): Pkg {
-    const indexFile = path.join(indexDirectory, `${name}.json`);
-    const indexData = fs.readFileSync(indexFile, 'utf8');
-    const index: Pkg = JSON.parse(indexData);
-    return index;
+  const indexFile = path.join(indexDirectory, `${name}.json`);
+  const indexData = fs.readFileSync(indexFile, "utf8");
+  const index: Pkg = JSON.parse(indexData);
+  return index;
 }
