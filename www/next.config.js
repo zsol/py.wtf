310a785da412418a1258dff796669459d94c3c3a
Zoltan Nagy <abesto0@gmail.com>
2022-08-09 18:40:58 +0000

    Increased largePageDataBytes threshold
    Before this change, we'd get lots of warnings about the data in
    `getStaticProps` being too large
    (https://nextjs.org/docs/messages/large-page-data). After thinking about
    it a bit: documentation for a module is sometimes going to be large,
    that's life. If / once we refactor the page structure this may get
    better, but even then, documentation for a _class_ can be big. And
    that's OK.
    
    [ghstack-poisoned]

--- www/next.config.js
+++ www/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    largePageDataBytes: 1024 * 1024,
+  },
+};
+
+module.exports = nextConfig;
