ae0cb7a47b8945a2bb565dc76a88c2d296e63ad0
Zoltan Nagy <abesto0@gmail.com>
2022-08-09 19:09:55 +0000

    [chore] Use next/link in ModuleListSidebar
    Without this, each click triggered a page reload. Now we do smart NextJS
    things instead.
    
    ghstack-source-id: d05f48a10685cb8fad2ca5392dfb6b0b70d68c9d
    Pull Request resolved: https://github.com/zsol/py.wtf/pull/11

--- www/components/ModuleListSidebar.tsx
+++ www/components/ModuleListSidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import NextLink from "next/link";
 import { Box } from "@welcome-ui/box";
 import { Text } from "@welcome-ui/text";
 import { Link } from "@welcome-ui/link";
@@ -7,7 +8,7 @@ import styled from "styled-components";
 
 import { Pkg } from "../lib/docs";
 
-const ModuleLink = styled(Link)<{ active: boolean }>`
+const StyledWUILink = styled(Link)<{ active: boolean }>`
   padding-left: ${(props) => props.theme.space.xs};
   padding-right: ${(props) => props.theme.space.md};
   width: 100%;
@@ -19,6 +20,12 @@ const ModuleLink = styled(Link)<{ active: boolean }>`
     color: ${props.theme.colors.dark["900"]};
   `}
 `;
+
+const ModuleLink = ({ children, href, active, ...rest }) => (
+  <NextLink href={href} passHref {...rest}>
+    <StyledWUILink active={active}>{children}</StyledWUILink>
+  </NextLink>
+);
 
 interface ModuleListSidebarProps {
   pkg: Pkg;
