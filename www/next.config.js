5a3be1f6db428280e25420ebb26071a8500d5287
Zoltan Nagy <abesto0@gmail.com>
2022-08-11 19:12:45 +0000

    (Try to) fix GH-flavored SPA

--- www/next.config.js
+++ www/next.config.js
@@ -8,12 +8,6 @@ const nextConfig = {
   experimental: {
     largePageDataBytes: 1024 * 1024,
   },
-  rewrites: async () => [
-    {
-      source: "/:any*",
-      destination: "/_spa",
-    },
-  ],
 };
 
 module.exports = nextConfig;
--- www/pages/_spa.tsx
+++ www/pages/_spa.tsx
@@ -5,7 +5,33 @@ import ModulePage from "@/components/SPA/ModulePage";
 import PackagePage from "@/components/SPA/PackagePage";
 import SymbolPage from "@/components/SPA/SymbolPage";
 
-export default function SPA() {
+export default function SPAIndex() {
+  /// Start Single Page Apps for GitHub Pages
+  const l = window.location;
+  const s = decodeURIComponent(l.search);
+  // Single Page Apps for GitHub Pages
+  // MIT License
+  // https://github.com/rafgraph/spa-github-pages
+  // This script checks to see if a redirect is present in the query string,
+  // converts it back into the correct url and adds it to the
+  // browser's history using window.history.replaceState(...),
+  // which won't cause the browser to attempt to load the new url.
+  // When the single page app is loaded further down in this file,
+  // the correct url will be waiting in the browser's history for
+  // the single page app to route accordingly.
+  if (s[1] === "/") {
+    const decoded: string = s
+      .slice(1)
+      .split("&")
+      .map(function (s) {
+        return s.replace(/~and~/g, "&");
+      })
+      .join("?")
+      .replace(/=$/g, "");
+    window.history.replaceState(null, "", decoded + l.hash);
+  }
+  //// End Single Page Apps for GitHub Pages
+
   return (
     <Router>
       <Routes>
