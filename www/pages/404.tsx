3d422bb97a0af22da4711e568df408a5223e8ed5
Zoltan Nagy <abesto0@gmail.com>
2022-08-11 19:22:49 +0000

    Try harder to fix GH-plavored SPA

--- www/pages/404.tsx
+++ www/pages/404.tsx
@@ -29,33 +29,10 @@ export default function SPARedirect() {
         .split("/")
         .slice(0, 1 + pathSegmentsToKeep)
         .join("/") +
-      "/_spa/?/" +
-      l.pathname
-        .slice(1)
-        .split("/")
-        .slice(pathSegmentsToKeep)
-        .join("/")
-        .replace(/&/g, "~and~") +
-      (l.search ? "&" + l.search.slice(1).replace(/&/g, "~and~") : "") +
-      l.hash
-  );
-  return null;
-  l.replace(
-    l.protocol +
-      "//" +
-      l.hostname +
-      (l.port ? ":" + l.port : "") +
-      l.pathname
-        .split("/")
-        .slice(0, 1 + pathSegmentsToKeep)
-        .join("/") +
-      "/_spa/?/" +
-      l.pathname
-        .slice(1)
-        .split("/")
-        .slice(pathSegmentsToKeep)
-        .join("/")
-        .replace(/&/g, "~and~") +
+      "/_spa?/" +
+      encodeURIComponent(
+        l.pathname.slice(1).split("/").slice(pathSegmentsToKeep).join("/")
+      ) +
       (l.search ? "&" + l.search.slice(1).replace(/&/g, "~and~") : "") +
       l.hash
   );
