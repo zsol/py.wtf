20087e3a46b4948081a1855fd557eac536677f92
Zoltan Nagy <abesto0@gmail.com>
2022-08-12 10:52:55 +0000

    Upgrade to React 18
    The changes in `pages/_app.tsx` implement a less-hacky way of separating
    out the server-side and client-side rendering; the
    `suppressHydrationWarning` attribute didn't actually suppress the
    badness sufficiently.
    
    ghstack-source-id: f729cd6db0285076ab7a2b9f5765a60b0092f6d0
    Pull Request resolved: https://github.com/zsol/py.wtf/pull/41

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { AppProps } from "next/app";
+import React, { useEffect, useState } from "react";
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof window === "undefined" ? null : <Component {...pageProps} />}
-    </div>
-  );
+  const [secondPass, setSecondPass] = useState(false);
+
+  useEffect(() => {
+    setSecondPass(true);
+  }, [secondPass]);
+
+  if (!secondPass) {
+    return null;
+  }
+
+  return <Component {...pageProps} />;
 }
 
 export default App;
