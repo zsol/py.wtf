d2e7467bb9209309f27a05b68a63af1f36ed71fa
Eva Csengeri-Pap <eva.csengeripap@noom.com>
2022-08-13 13:12:40 +0100

    Added basic reusable components for typography

---
+++ www/components/core/typography/Heading.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const H3 = styled.h3`
+  color: ${(props) => props.theme.colors.mainText};
+  font-size: 1.5em;
+`;
---
+++ www/components/core/typography/Text.tsx
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const Text = styled.p`
+  color: ${(props) => props.theme.colors.mainText};
+`;
