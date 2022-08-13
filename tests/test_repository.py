a7510ebd6efbc23d6be6f9edf189184abdd85c3f
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-08-13 17:40:39 +0100

    feat: add a project repository to the indexer
    This acts as a cache layer between the indexer and the filesystem. In the future, the
    indexer will ask for a project's dependencies from here for cross-referencing purposes.
    
    [ghstack-poisoned]

---
+++ tests/test_repository.py
@@ -0,0 +1,79 @@
+from dataclasses import replace
+from pathlib import Path
+from typing import Iterable
+
+import pytest
+from py_wtf.repository import ProjectRepository
+from py_wtf.types import Project, ProjectMetadata, ProjectName
+
+
+@pytest.fixture
+def project() -> Project:
+    return Project(
+        name=ProjectName("testproject"),
+        metadata=ProjectMetadata(
+            version="1.1.1.1.1",
+            classifiers=None,
+            home_page=None,
+            license=None,
+            documentation_url=None,
+            dependencies=[],
+            summary=None,
+        ),
+        modules=[],
+        documentation=[],
+    )
+
+
+@pytest.fixture
+def repo(tmp_path: Path) -> ProjectRepository:
+    return ProjectRepository(tmp_path)
+
+
+def test_empty_raises(repo: ProjectRepository) -> None:
+    with pytest.raises(KeyError):
+        repo[ProjectName("foo")]
+
+
+def test_get_calls_factory_once(repo: ProjectRepository, project: Project) -> None:
+    called = False
+
+    def factory(key: ProjectName) -> Iterable[Project]:
+        nonlocal called
+        called = True
+        yield project
+
+    ret = repo.get(project.name, factory)
+    assert called
+    assert ret == project
+
+    called = False
+
+    ret = repo.get(project.name, factory)
+    assert not called
+    assert ret == project
+
+
+def test_index_works(repo: ProjectRepository, project: Project) -> None:
+    repo._save(project)
+    assert repo[project.name] == project
+
+
+def test_save_writes_to_disk(repo: ProjectRepository, project: Project) -> None:
+    repo._save(project)
+    assert (repo.directory / f"{project.name}.json").exists()
+
+
+def test_getitem_loads_from_disk(repo: ProjectRepository, project: Project) -> None:
+    index_file = repo.directory / f"{project.name}.json"
+    index_file.write_text(project.to_json())  # type: ignore
+    assert repo[project.name] == project
+
+
+def test_double_save(repo: ProjectRepository, project: Project) -> None:
+    other_project = replace(project, documentation=["foo"])
+    assert project != other_project
+    repo._save(project)
+    assert repo[project.name] == project
+    repo._save(other_project)
+    assert repo[other_project.name] == project
