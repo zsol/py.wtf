3fa6925bd6446bfa5435898b5f2dbf697198e30d
Eva Csengeri-Pap <eva.csengeripap@noom.com>
2022-08-13 17:03:59 +0100

    Fixed disappearing padding on code blocks

--- www/components/core/typography/Code.tsx
+++ www/components/core/typography/Code.tsx
@@ -3,13 +3,17 @@ import { ReactNode } from "react";
 
 const CodeContainer = styled.pre`
   width: 100%;
+  padding-top: ${(props) => props.theme.spacing.m};
+  padding-bottom: ${(props) => props.theme.spacing.m};
 
   color: ${(props) => props.theme.colors.mainText};
   background-color: ${(props) => props.theme.colors.pageBackground.dark};
 `;
 
 const CodeContent = styled.code`
-  margin: ${(props) => props.theme.spacing.m};
+  display: block;
+  padding-left: ${(props) => props.theme.spacing.m};
+  padding-right: ${(props) => props.theme.spacing.m};
 `;
 
 export const Code = ({ children }: { children: ReactNode }) => (
