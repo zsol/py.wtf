a5dbeb6f69259d069296efb1be959d199110c323
Eva Csengeri-Pap <eva.csengeripap@noom.com>
2022-08-16 21:08:53 +0100

    Added a component handling the header and footer and applied it to index

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,15 +1,10 @@
-import { ThemeProvider } from "@emotion/react";
-import styled from "@emotion/styled";
 import { GetStaticProps } from "next";
-import Head from "next/head";
 
 import SymbolLinkTable from "@/components/Docs/SymbolLinkTable";
-import FullPageBackground from "@/components/core/layout/FullPageBackground";
+import PageLayout from "@/components/PageLayout";
 import { flexColumnCenter } from "@/components/core/layout/helpers";
-import { Link } from "@/components/core/navigation/Link";
-import { darkTheme } from "@/components/core/theme/theme";
 
-import { Project, ProjectMetadata, getProject, listProjects } from "@/lib/docs";
+import { Project, getProject, listProjects } from "@/lib/docs";
 import * as url from "@/lib/url";
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
@@ -29,47 +24,19 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     },
   };
 };
-
-const Content = styled.main`
-  ${flexColumnCenter}
-  flex: 1;
-
-  padding: 5rem 0;
-`;
-
-const Footer = styled.footer`
-  ${flexColumnCenter}
-
-  width: 100%;
-  height: 100px;
-
-  border-top: 1px solid #eaeaea;
-`;
-
 export interface Props {
   projects: { name: string; documentation: string[] }[];
 }
 
 export default function Home({ projects }: Props) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <FullPageBackground>
-        <Head>
-          <title>py.wtf</title>
-          {/*<link rel="icon" href="/favicon.ico" /> */}
-        </Head>
-
-        <Content>
-          <SymbolLinkTable
-            title="Projects"
-            url={(prj) => url.project(prj as Project)}
-            symbols={projects}
-            useReactRouter={false}
-          />
-        </Content>
-
-        <Footer>Footer.</Footer>
-      </FullPageBackground>
-    </ThemeProvider>
+    <PageLayout>
+      <SymbolLinkTable
+        title="Projects"
+        url={(prj) => url.project(prj as Project)}
+        symbols={projects}
+        useReactRouter={false}
+      />
+    </PageLayout>
   );
 }
