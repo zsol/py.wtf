e4c7c08c9b490b6fe17784238e7db1aec00ef44b
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-08-28 16:54:36 +0100

    feat: parse RST into MyST

---
+++ py_wtf/indexer/documentation.py
@@ -0,0 +1,19 @@
+import re
+from inspect import cleandoc
+
+import rst_to_myst
+
+from py_wtf.types import Documentation
+
+RST_ROLE = re.compile(r":[a-zA-Z]+:`")
+
+
+def convert_to_myst(src: str) -> Documentation:
+    text = cleandoc(src)
+    if is_rst(text):
+        text = rst_to_myst.rst_to_myst(src).text
+    return Documentation(text)
+
+
+def is_rst(src: str) -> bool:
+    return bool(RST_ROLE.finditer(src))
---
+++ tests/indexer/test_documentation.py
@@ -0,0 +1,29 @@
+from py_wtf.indexer.documentation import convert_to_myst, is_rst
+
+
+def test_simple_rst() -> None:
+    docstring = """Break *iterable* into lists of length *n*:
+
+    >>> list(chunked([1, 2, 3, 4, 5, 6], 3))
+    [[1, 2, 3], [4, 5, 6]]
+
+By the default, the last yielded list will have fewer than *n* elements
+if the length of *iterable* is not divisible by *n*:
+
+    >>> list(chunked([1, 2, 3, 4, 5, 6, 7, 8], 3))
+    [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+To use a fill-in value instead, see the :func:`grouper` recipe.
+If the length of *iterable* is not divisible by *n* and *strict* is
+``True``, then ``ValueError`` will be raised before the last
+list is yielded.
+
+    """
+    result = convert_to_myst(docstring)
+    assert "{func}`grouper`" in result
+    # four lines of total blockquotes
+    assert result.count("\n> ") == 4
+
+
+def test_is_rst() -> None:
+    assert is_rst("foo :code:`lol`")
