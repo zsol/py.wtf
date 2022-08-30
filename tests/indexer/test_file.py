367b117b79785f2fa8308248a6f05dd3f042ae30
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-08-30 21:12:32 +0100

    feat: parse RST into MyST
    ghstack-source-id: 49272afc0b05f4a694700b6a329e44b829ea3608
    Pull Request resolved: https://github.com/zsol/py.wtf/pull/96

--- tests/indexer/test_file.py
+++ tests/indexer/test_file.py
@@ -189,14 +189,17 @@ def test_index_package_exports_with_dependencies(package_with_dep: Module) -> No
 
 
 def test_module_docs(mod: Module) -> None:
-    assert mod.documentation == [Documentation("module docs"), Documentation("header")]
+    assert mod.documentation == [
+        Documentation("module docs\n"),
+        Documentation("header"),
+    ]
 
 
 def test_func_docs(mod: Module) -> None:
     func = [f for f in mod.functions if f.name.endswith(".func")][0]
-    assert func.documentation == [Documentation("func docs")]
+    assert func.documentation == [Documentation("func docs\n")]
 
 
 def test_class_docs(mod: Module) -> None:
     cls = mod.classes[0]
-    assert cls.documentation == [Documentation("class docs")]
+    assert cls.documentation == [Documentation("class docs\n")]
